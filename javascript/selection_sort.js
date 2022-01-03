816bd5839ed5932147eb35e885a0fdb42b9e1915
Ryan Burns <ryankburns@gmail.com>
2022-01-02 16:44:13 -0800

    solved sorted array with benchmarking

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,6 +1,11 @@
 function selectionSort(arr) {
   // type your code here
+  sortedArr = []
+  sortedArr = arr.sort((a, b) => {return a - b})
+  return sortedArr
 }
+
+selectionSort([1, 3, 2, -1])
 
 if (require.main === module) {
   // add your own tests in here
@@ -10,14 +15,28 @@ if (require.main === module) {
   console.log("");
 
   // BENCHMARK HERE, and print the average runtime
+  for (let i = 0; i < 1000; ++i) {
   const longInput = [];
-
   for (let i = 0; i < 100; ++i) {
-    longInput.push(Math.random());
+    longInput.push(Math.floor(Math.random()*1000));
+  }
+
+  let startTime = new Date;
+
+  selectionSort(longInput)
+
+  let endTime = new Date;
+
+  let benchMark = (endTime - startTime)
+
+  console.log(benchMark)
   }
 }
 
 module.exports = selectionSort;
 
 // Please add your pseudocode to this file
+  //create new array
+  //assign given array to new array once sorted
+  //return sorted array
 // And a written explanation of your solution
